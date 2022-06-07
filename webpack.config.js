62682fb698a84885bb06fc7723f9fd439500314a
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-07 17:31:30 +0100

    Add Webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
