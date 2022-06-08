cfd3930192bf508feba6e4bd4bf95d3f751321fe
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-08 12:08:08 +0100

    Add Class and add methods

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 import './style.css';
 
 // Queries to HTML
-const section = document.querySelector('section');
-const inputText = document.querySelector('input');
-const todoMainContainer = document.querySelector('.todos-container');
-const ClearBtn = document.querySelector('button');
+const section = document.querySelector("section");
+const inputText = document.querySelector("input");
+const todoMainContainer = document.querySelector(".todos-container");
+const ClearBtn = document.querySelector("button");
 
+// class object
 class MyObjects {
   constructor(description, completed, index) {
     this.description = description;
@@ -16,4 +17,22 @@ class MyObjects {
 }
 
 const myArray = [];
-const todosContainer = document.querySelector
+const addTodo = (todoValue) => {
+  const todoContainer = document.createElement('div');
+  todoContainer.className = 'todoContainer';
+  todoContainer.innerHTML = `
+    <input type='checkbox' class='checkbox';
+    <span>${todoValue}</span>
+    <i class="fas fa-ellipsis-v"></i>
+    <i class="fas fa-trash-alt"></i>
+  `;
+  todoMainContainer.appendChild(todoContainer);
+};
+
+inputText.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && inputText.value) {
+    e.preventDefault();
+    addTodo(inputText.value);
+    inputText.value = null;
+  }
+});
