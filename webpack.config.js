f4d8ea0cfb84e5386704507a99bad46823cf2166
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-08 22:06:21 +0100

    Fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   mode: 'development',
+
+  entry: './src/index.js',
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
     ],
