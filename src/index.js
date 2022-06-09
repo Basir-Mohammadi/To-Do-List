c142a58f08130228e12a85330c43d8c46c390dc0
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-09 21:26:28 +0100

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ const removeTodo = (todo) => {
   todoMainContainer.removeChild(todo);
   let count = 0;
   const DataFromLocalStorage = JSON.parse(localStorage.getItem('list'));
-  function complete(i) { i.completed = false };
+  function complete(i) { i.completed = false; };
   const data = Array.from(DataFromLocalStorage).filter(complete);
-  function counts(i) { i.index = count += 1 };
+  function counts(i) { i.index = count + 1; }
   data.map(counts);
   localStorage.setItem(('list'), JSON.stringify(data));
 };
